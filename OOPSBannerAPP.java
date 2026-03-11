521e9a3e710a02a08acec92ed4f939b4fea07e22
anandpiyush121-goat <anandpiyush121@gmail.com>
2026-03-11 09:23:39 +0530

    UC6

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,15 +1,16 @@
 public class OOPSBannerAPP {
     public static void main(String[] args) {
+        String [] lines = {
+            String.join(" ", "   *****   ", "    *****   ", " ********  ", "  ******  "),
+            String.join(" ", " **     **  ", " **     **  ", "**    ** ", " **      **"),
+            String.join(" ", "**       ** ", "**       ** ", "**    **  ", "**       "),
+            String.join(" ", "**       ** ", "**       ** ", "******** ", "   ******  "),
+            String.join(" ", "**       ** ", "**       ** ", "**        ", "        ** "),
+            String.join(" ", " **     **  ", " **     **  ", "**        ", "**      ** "),
+            String.join(" ", "   *****   ", "    *****   ", " **        ", "  ******  ")
+        };
 
-        String[] banner = new String[5];
-
-        banner[0] = String.join(" ", " ***  ", " ***  ", " **** ", " **** ");
-        banner[1] = String.join(" ", "*   * ", "*   * ", "*   * ", " *    ");
-        banner[2] = String.join(" ", "*   * ", "*   * ", "****  ", " **** ");
-        banner[3] = String.join(" ", "*   * ", "*   * ", "*     ", "    * ");
-        banner[4] = String.join(" ", " ***  ", " ***  ", "*     ", " **** ");
-
-        for (String line : banner) {
+        for (String line : lines) {
             System.out.println(line);
         }
     }
