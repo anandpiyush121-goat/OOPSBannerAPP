4adf8a851960fde5b93a24b66e8153eb249c89b6
anandpiyush121-goat <anandpiyush121@gmail.com>
2026-03-11 09:36:16 +0530

    UC7

--- OOPSBannerAPP.java
+++ OOPSBannerAPP.java
@@ -1,15 +1,65 @@
 public class OOPSBannerAPP {
+
+    // Static inner class to store character and its pattern
+    static class CharacterPatternMap {
+        private char character;
+        private String[] pattern;
+
+        // Constructor
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join(" ", " ***  ", " ***  ", " **** ", " **** "),
-                String.join(" ", "*   * ", "*   * ", "*   * ", " *    "),
-                String.join(" ", "*   * ", "*   * ", "****  ", " **** "),
-                String.join(" ", "*   * ", "*   * ", "*     ", "    * "),
-                String.join(" ", " ***  ", " ***  ", "*     ", " **** ")
-        };
+        CharacterPatternMap O = new CharacterPatternMap('O', new String[]{
+                "  ****  ",
+                " **  ** ",
+                "**    **",
+                "**    **",
+                "**    **",
+                " **  ** ",
+                "  ****  "
+        });
 
-        for (String line : banner) {
+        CharacterPatternMap P = new CharacterPatternMap('P', new String[]{
+                "****** ",
+                "**   **",
+                "****** ",
+                "**     ",
+                "**     ",
+                "**     ",
+                "**     "
+        });
+
+        CharacterPatternMap S = new CharacterPatternMap('S', new String[]{
+                " ******",
+                "**     ",
+                "**     ",
+                " ***** ",
+                "     **",
+                "     **",
+                "****** "
+        });
+
+        String[] oPattern = O.getPattern();
+        String[] pPattern = P.getPattern();
+        String[] sPattern = S.getPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            StringBuilder line = new StringBuilder();
+            line.append(oPattern[i]).append(" ");
+            line.append(oPattern[i]).append(" ");
+            line.append(pPattern[i]).append(" ");
+            line.append(sPattern[i]);
+
             System.out.println(line);
         }
     }
